d94b18ec4ea89637432f517b79c94da3b1f025d4
sugarshin <shinsugar@gmail.com>
2019-07-09 23:22:11 +0900

    Rename

--- app/purge.go
+++ app/purge.go
@@ -14,42 +14,42 @@ type Result struct {
 	Status int
 }
 
-func purge(imgSrc string, baseURL string) Result {
-	imgSrcURL := ""
-	if path.IsAbs(imgSrc) {
+func purge(resource string, baseURL string) Result {
+	resourceURL := ""
+	if path.IsAbs(resource) {
 		base, err := url.Parse(baseURL)
 		if err != nil {
 			return Result{URL: "", Status: 0}
 		}
-		u, err := url.Parse(imgSrc)
+		u, err := url.Parse(resource)
 		if err != nil {
 			return Result{URL: "", Status: 0}
 		}
-		imgSrcURL = base.ResolveReference(u).String()
-	} else if isURI(imgSrc) {
-		imgSrcURL = imgSrc
+		resourceURL = base.ResolveReference(u).String()
+	} else if isURI(resource) {
+		resourceURL = resource
 	} else {
 		ext := path.Ext(baseURL)
 		if ext != "" {
 			dir, _ := path.Split(baseURL)
-			imgSrcURL = regexp.MustCompile(`/$`).ReplaceAllString(dir, "") + "/" + path.Clean(imgSrc)
+			resourceURL = regexp.MustCompile(`/$`).ReplaceAllString(dir, "") + "/" + path.Clean(resource)
 		} else {
-			imgSrcURL = regexp.MustCompile(`/$`).ReplaceAllString(baseURL, "") + "/" + path.Clean(imgSrc)
+			resourceURL = regexp.MustCompile(`/$`).ReplaceAllString(baseURL, "") + "/" + path.Clean(resource)
 		}
 	}
 
 	client := http.Client{}
-	req, err := http.NewRequest("PURGE", imgSrcURL, nil)
+	req, err := http.NewRequest("PURGE", resourceURL, nil)
 	if err != nil {
 		log.Fatal(err)
-		return Result{URL: imgSrcURL, Status: 0}
+		return Result{URL: resourceURL, Status: 0}
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
-		return Result{URL: imgSrcURL, Status: 0}
+		return Result{URL: resourceURL, Status: 0}
 	}
 
-	return Result{URL: imgSrcURL, Status: res.StatusCode}
+	return Result{URL: resourceURL, Status: res.StatusCode}
 }
